1085ece9f72b534a8adc5891b43257f8f971374b
hendry <hendrry16@gmail.com>
2023-04-09 11:59:15 +0000

    install eslint

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <img src="https://storage.cloud.google.com/hendry16-bucket/sign.png" alt="" />
     </div>
   );
 }
